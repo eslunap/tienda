9a44310c152038f94f113318bdc01bc6edbfbbb5
eslunap19 <estefany.luna@gmail.com>
2023-05-20 02:14:05 -0500

    creación método consulta nombre

--- src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
+++ src/main/java/com/latam/alura/tienda/dao/ProductoDao.java
@@ -25,4 +25,9 @@ public class ProductoDao {
         String jpql = "SELECT P FROM Producto AS P";
         return em.createQuery(jpql, Producto.class).getResultList();
     }
+
+    public List<Producto> consultaPorNombre(String nombre){
+        String jpql ="SELECT P FROM Producto AS P WHERE P.nombre =:nombre";
+        return em.createQuery(jpql).setParameter("nombre", nombre).getResultList();
+    }
 }
