a433c5a4f29d061092835fb232219a403d5eb488
eslunap19 <estefany.luna@gmail.com>
2023-05-20 00:24:12 -0500

    Create CategoriaDao.java

---
+++ src/main/java/com/latam/alura/tienda/dao/CategoriaDao.java
@@ -0,0 +1,19 @@
+package com.latam.alura.tienda.dao;
+
+import com.latam.alura.tienda.modelo.Categoria;
+import com.latam.alura.tienda.modelo.Producto;
+
+import javax.persistence.EntityManager;
+
+public class CategoriaDao {
+
+    private EntityManager em;
+
+    public CategoriaDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void guardar(Categoria categoria){
+        this.em.persist(categoria);
+    }
+}
