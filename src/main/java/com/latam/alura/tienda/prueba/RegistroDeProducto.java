4fd6fe9e79c9c1d0803748309cfb83383de7f3d5
eslunap19 <estefany.luna@gmail.com>
2023-05-20 01:28:16 -0500

    metodos flush clear

--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -14,19 +14,22 @@ import java.math.BigDecimal;
 public class RegistroDeProducto {
     public static void main(String[] args) {
         Categoria celulares = new Categoria("CELULARES");
-        Producto celular = new Producto("Samsung", "teléfono usado", new BigDecimal("1000"), celulares);
+        //Producto celular = new Producto("Samsung", "teléfono usado", new BigDecimal("1000"), celulares);
 
         EntityManager em = JPAUtils.getEntityManager();
 
-        ProductoDao productoDao = new ProductoDao(em);
-        CategoriaDao categoriaDao = new CategoriaDao(em);
-
         em.getTransaction().begin();
 
-        productoDao.guardar(celular);
-        categoriaDao.guardar(celulares);
+        em.persist(celulares);
 
-        em.getTransaction().commit();
-        em.close();
+        celulares.setNombre("LIBROS");
+
+        em.flush();
+        em.clear();
+
+        celulares = em.merge(celulares);
+        celulares.setNombre("SOFTWARES");
+
+        em.flush();
     }
 }
