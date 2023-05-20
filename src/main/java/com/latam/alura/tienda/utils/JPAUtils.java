0e7f00755c651a26c2549d0c6b623dc42df1a5b3
eslunap19 <estefany.luna@gmail.com>
2023-05-19 23:50:50 -0500

    Create JPAUtils.java

---
+++ src/main/java/com/latam/alura/tienda/utils/JPAUtils.java
@@ -0,0 +1,13 @@
+package com.latam.alura.tienda.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtils {
+    private static EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("tienda-alura");
+
+    public static EntityManager getEntityManager(){
+        return FACTORY.createEntityManager();
+    }
+}
