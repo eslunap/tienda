1e7be5f8642e08bed885234e5c39b38787238d6f
eslunap <estefany.luna@gmail.com>
2023-05-21 00:52:45 -0500

    Update ItemsPedido.java
    creación constructores y métodos

--- src/main/java/com/latam/alura/tienda/modelo/ItemsPedido.java
+++ src/main/java/com/latam/alura/tienda/modelo/ItemsPedido.java
@@ -19,4 +19,48 @@ public class ItemsPedido {
     @ManyToOne
     private Pedido pedido;
 
+    public ItemsPedido() {
+    }
+
+    public ItemsPedido(int cantidad, Producto producto, Pedido pedido) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+        this.pedido = pedido;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
 }
