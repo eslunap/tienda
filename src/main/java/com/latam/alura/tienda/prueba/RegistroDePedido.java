da5e1f21d3ce34a9d0ec996257f4ba0419187be7
eslunap <estefany.luna@gmail.com>
2023-05-21 20:57:14 -0500

    run application
    instancia de clase ClienteDao

--- src/main/java/com/latam/alura/tienda/prueba/RegistroDePedido.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDePedido.java
@@ -1,6 +1,7 @@
 package com.latam.alura.tienda.prueba;
 
 import com.latam.alura.tienda.dao.CategoriaDao;
+import com.latam.alura.tienda.dao.ClienteDao;
 import com.latam.alura.tienda.dao.PedidoDao;
 import com.latam.alura.tienda.dao.ProductoDao;
 import com.latam.alura.tienda.modelo.*;
@@ -17,11 +18,19 @@ public class RegistroDePedido {
         ProductoDao productoDao = new ProductoDao(em);
         Producto producto = productoDao.consultaPorId(1L);
 
+        ClienteDao clienteDao = new ClienteDao(em);
         PedidoDao pedidoDao = new PedidoDao(em);
 
         Cliente cliente = new Cliente("Juan", "12345678");
         Pedido pedido = new Pedido(cliente);
         pedido.agregarItems(new ItemsPedido(5, producto, pedido));
+
+        em.getTransaction().begin();
+
+        clienteDao.guardar(cliente);
+        pedidoDao.guardar(pedido);
+
+        em.getTransaction().commit();
 
 
     }
