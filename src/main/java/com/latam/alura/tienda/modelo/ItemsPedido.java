e5a6423dd195101173e41e22806dfffad1baa3c5
eslunap <estefany.luna@gmail.com>
2023-05-24 15:17:03 -0500

    Update ItemsPedido.java
    creación método GetValor

--- src/main/java/com/latam/alura/tienda/modelo/ItemsPedido.java
+++ src/main/java/com/latam/alura/tienda/modelo/ItemsPedido.java
@@ -64,4 +64,8 @@ public class ItemsPedido {
     public void setPedido(Pedido pedido) {
         this.pedido = pedido;
     }
+
+    public BigDecimal getValor() {
+        return this.precioUnitario.multiply(new BigDecimal(this.cantidad));
+    }
 }
