bf64ef8507c974a479bc5cbf4e69b7ed74fcd67b
eslunap <estefany.luna@gmail.com>
2023-05-27 01:47:11 -0500

    Update inheritance type en Producto

--- src/main/java/com/latam/alura/tienda/modelo/Producto.java
+++ src/main/java/com/latam/alura/tienda/modelo/Producto.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name="productos")
 @NamedQuery(name="Producto.consultaDePrecio", query = "SELECT P.precio FROM Producto AS P WHERE P.nombre=:nombre")
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Producto{
 
     @Id
